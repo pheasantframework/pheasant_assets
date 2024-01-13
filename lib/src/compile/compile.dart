81d554ee74fe8c330bd515d9d56b9adeed10ec33
Nike <nikechukwu@gmail.com>
2024-01-13 11:18:17 +0100

    Documentation

--- lib/src/compile/compile.dart
+++ lib/src/compile/compile.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:csslib/parser.dart' as css;
@@ -8,18 +7,24 @@ import 'package:sass/sass.dart' as sass;
 import '../exceptions/exceptions.dart';
 import '../sass/logger.dart';
 import '../assets.dart';
+import '../constants/syntax.dart' as syn;
 
 /// Function used to compile css.
 /// This function is the second step after the `css()` function, when it comes to compiling css. 
 /// 
 /// Here, the [PheasantStyle] object only serves the purpose of denoting the configuration (`syntax` and so on) of the compiler function.
 /// 
-/// The [cssString] represents the string to be compiled. 
+/// The [cssString] represents the string to be compiled.
 /// 
-/// Returns the compiled css as a String, throws an error if it didn't compile.
+/// The function first of all confirms the syntax given is valid, via the [syn.syntax] list, and throws a [PheasantStyleException] if it is not included.
+/// 
+/// Afterwards, the function is compiled. If the syntax is already css, then the [css.compile] compiler has the sole purpose of validating the code. 
+/// If the css is invalid, then it will write out [css.Message] messages, and all will be written to the console, then [PheasantStyleException] will be thrown.
+/// 
+/// If the synax isn't css, then the [sass] compiler will compile the css. Returns the compiled css as a String, throws a [PheasantStyleException] if the code didn't compile.
 String compileCss(PheasantStyle pheasantStyle, String cssString) {
   var cssErrors = <css.Message>[];
-  if (!['css', 'sass', 'scss'].contains(pheasantStyle.syntax)) {
+  if (!syn.syntax.contains(pheasantStyle.syntax)) {
     throw PheasantStyleException('Syntax is invalid: ${pheasantStyle.syntax}');
   }
 
@@ -29,10 +34,10 @@ String compileCss(PheasantStyle pheasantStyle, String cssString) {
       errors: cssErrors,
     );
     if (cssErrors.isNotEmpty) {
-      cssErrors.forEach((element) {
-        log("Error: ${element.message}");
-        log("Error Description: ${element.describe}");
-      });
+      for (var element in cssErrors) {
+        print("Error compiling css: ${element.message}");
+        print("Error Description: ${element.describe}");
+      }
       throw PheasantStyleException("Error(s) Occured while Compiling CSS: ${cssErrors.map((e) => e.message)}\n $pheasantStyle");
     } else {
       return cssString;
@@ -51,9 +56,21 @@ String compileCss(PheasantStyle pheasantStyle, String cssString) {
   }
 }
 
+/// Function to compile css given the file path.
+/// 
+/// This is similar to the [compileCss] function, but this compiles given the file path, rather than a raw string. So all parts except the final compilation are similar.
+/// 
+/// The css, regardless of syntax, is compiled with the [sass] compiler, 
+/// and on the throw of a [sass.SassException], the function will then also throw a [PheasantStyleException].
+/// 
+/// The [sassPath] contains the relative path of the css/sass/scss file to the pheasant file.
+/// 
+/// The [componentDirPath] is the directory path of the pheasant file (or the css/sass/scss file) relative to the project. It defaults to `'lib'`.
+/// 
+/// The [devDirPath] is exclusively a gateway for the example code of this project, and should not be used outside the api.
 String compileSassFile(PheasantStyle pheasantStyle, String sassPath, {String componentDirPath = 'lib', String? devDirPath}) {
   String absPath = join(Directory.current.path, (devDirPath ?? componentDirPath), sassPath);
-  if (!['css', 'sass', 'scss'].contains(pheasantStyle.syntax)) {
+  if (!syn.syntax.contains(pheasantStyle.syntax)) {
     throw PheasantStyleException('Syntax is invalid: ${pheasantStyle.syntax}');
   }
 
--- lib/src/compile/parse.dart
+++ lib/src/compile/parse.dart
@@ -1,5 +1,12 @@
 import '../assets.dart';
 
+/// Code to extract a [PheasantStyle] from a `style` component in a pheasant file.
+/// 
+/// The function extracts relevant information about the component using the [RegExp].
+/// The data from the opening tag and the internal data is then processed and then used to create a new [PheasantStyle] object.
+/// 
+/// If [sassEnabled] is set to true, then the `syntax` of the [PheasantStyle] can be adjusted. Else, the `syntax` is set to `"css"` despite what may have been denoted in the style tag.
+/// By default, [sassEnabled] is set to `true`.
 PheasantStyle getStyleInput(String style, {bool sassEnabled = true}) {
   RegExp regex = RegExp(
     r'<style\s*' // Match the opening tag '<style' and one or more whitespaces
