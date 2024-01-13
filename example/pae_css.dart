9f54dbe2244f9cc12ffd4ac5cda88b724dc1581c
Nike <nikechukwu@gmail.com>
2024-01-13 11:45:52 +0100

    Renamed CSS File

--- example/pae_css.dart
+++ example/pae_css.dart
@@ -1,7 +1,7 @@
 import 'package:pheasant_assets/pheasant_assets.dart';
 
 void main() {
-  PheasantStyle myStyle = getStyleInput('<style src="foo.css"></style>');
+  PheasantStyle myStyle = getStyleInput('<style src="styles.css"></style>');
   PheasantStyleScoped scopedStyle = scopeComponents(myStyle, isDev: true);
   print("CSS Data: ${scopedStyle.css}");
   print("CSS Scoping ID: ${scopedStyle.id}");
